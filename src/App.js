54f82a99f5d03605e480ac56a65e451bc9399a3a
Ishara Dissanayake <thisismaduishara@gmail.com>
2022-12-18 15:28:55 +0530

    major refinements

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo_bg.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          This site is under <code>maintainance</code>
+        </p>
+        <a
+          className="App-link"
+          href="https://github.com/thisisishara"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Visit GitHub page for now
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
