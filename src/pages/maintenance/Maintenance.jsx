ffcd76de59ddb976d8499d491a4e4f4c19971671
Ishara Dissanayake <thisismaduishara@gmail.com>
2022-12-20 10:31:01 +0530

    latest maintenance build

---
+++ src/pages/maintenance/Maintenance.jsx
@@ -0,0 +1,23 @@
+import logo from '../../res/logo.png';
+import './Maintenance.css';
+
+export default function Maintenance() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          This site is under <code>maintenance</code>
+        </p>
+        <a
+          className="App-link"
+          href="https://github.com/thisisishara"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Visit the <code>GitHub</code> page for now
+        </a>
+      </header>
+    </div>
+  );
+};
