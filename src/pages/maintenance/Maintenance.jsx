2a5afc95646751c969ec26c1bc9ccbee913e5dfd
Ishara Dissanayake <thisismaduishara@gmail.com>
2023-07-09 05:10:49 +0530

    switch to maintenance mode

--- src/pages/maintenance/Maintenance.jsx
+++ src/pages/maintenance/Maintenance.jsx
@@ -1,11 +1,11 @@
-import logo from '../../res/logo.png';
+// import logo from '../../res/logo.png';
 import './Maintenance.css';
 
 export default function Maintenance() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>
           This site is under <code>maintenance</code>
         </p>
@@ -15,7 +15,7 @@ export default function Maintenance() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Visit the <code>GitHub</code> page for now
+          Visit the <code>GitHub</code> profile for now
         </a>
       </header>
     </div>
